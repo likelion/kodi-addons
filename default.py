1cd8b58aa52c8125b96005187d375ddc2a32b822
Leonid Mokrushin <leonid.mokrushin@ericsson.com>
2017-01-04 16:37:36 +0100

    first try to implement seek

--- default.py
+++ default.py
@@ -9,14 +9,18 @@ from urllib import urlencode
 from urlparse import parse_qsl
 import urllib2
 import json
+import xbmc
 import xbmcaddon
 import xbmcgui
 import xbmcplugin
+import re
 
 # Get the plugin url in plugin:// notation.
 _url = sys.argv[0]
 # Get the plugin handle as an integer number.
 _handle = int(sys.argv[1])
+
+#xbmc.log("ARGV = "+str(sys.argv))
 
 _api = 'https://iptv.kartina.tv/api/json/'
 
@@ -54,14 +58,90 @@ def get_url(**kwargs):
     return '{0}?{1}'.format(_url, urlencode(kwargs))
 
 def list_channels(sid):
-    doc = api_call(sid, 'channel_list', show='all', protect_code=get_setting('password'))
+    doc = api_call(sid, 'channel_list', show='all', protect_code=get_setting('protect'))
     for group in doc.get('groups'):
         for channel in group.get('channels'):
-            list_item = xbmcgui.ListItem(label=channel.get('name'))
-            list_item.setArt({'thumb': channel.get('icon_link')})
-            url = get_url(action='listing', sid=sid)
-            xbmcplugin.addDirectoryItem(_handle, url, list_item, True)
+            if channel.get('is_video') == 1:
+                label = channel.get('name')
+                arch = channel.get('have_archive')
+                if arch == 1:
+                    label = '[COLOR red]'+u"\u2022"+'[/COLOR] '+label
+                else:
+                    label = '[COLOR gray]'+u"\u2022"+'[/COLOR] '+label
+                list_item = xbmcgui.ListItem(label=label)
+                list_item.setArt({'thumb': channel.get('icon_link')})
+                url = get_url(action='play', cid=channel.get('id'), sid=sid, arch=arch)
+                xbmcplugin.addDirectoryItem(_handle, url, list_item, False)
+    xbmc.executebuiltin('Container.SetViewMode(51)')
     xbmcplugin.endOfDirectory(_handle)
+
+
+class MyMonitor(xbmc.Monitor):
+
+    def __init__ (self):
+        xbmc.Monitor.__init__(self)
+
+    def onAbortRequested(self):
+        global closescript
+        closescript = True
+
+
+class MyPlayer(xbmc.Player):
+
+    def __init__ (self, sid, cid, arch):
+        xbmc.Player.__init__(self)
+        self.sid = sid
+        self.cid = cid
+        self.arch = arch
+        self.gmt = -1
+
+    def play_channel(self):
+        if self.gmt == -1:
+            doc = api_call(self.sid, 'settings', var='stream_standard')
+            self.max = int(doc.get('servertime'))
+            length = doc.get('settings').get('list')[0].get('catchup').get('length')
+            self.min = self.max - length
+            self.gmt = self.max-60*60
+        else:
+            if self.gmt < self.min:
+                self.gmt = self.min
+            elif self.gmt > self.max:
+                self.gmt = self.max
+        #xbmc.log('sid='+self.sid+' cid='+str(self.cid))
+        #xbmc.log('--------- playing from '+str(self.gmt))
+        if self.arch == '1':
+            doc = api_call(self.sid, 'get_url', cid=self.cid, gmt=self.gmt, protect_code=get_setting('protect'))
+        else:
+            doc = api_call(self.sid, 'get_url', cid=self.cid, protect_code=get_setting('protect'))
+        url = doc.get('url')
+        #xbmc.log('url='+str(url))
+        url = re.sub('http/ts(.*?)\s(.*)', 'http\\1', url)
+        li = xbmcgui.ListItem('My program')
+        li.addStreamInfo('video', {'width':331, 'duration': 1801})
+
+        self.play(url, li)
+
+    def onPlayBackStopped(self):
+        global closescript
+        closescript = True
+        #xbmc.log("stopped!!!!!!")
+
+    def onPlayBackSeek(self, time, seekOffset):
+        if self.arch == '1':
+            self.pause()
+            self.gmt = self.gmt + int(self.getTime()) + int(seekOffset/1000)
+            #xbmc.log('--------- seeking '+str(int(seekOffset/1000))+' to '+str(self.gmt))
+            self.play_channel()
+
+closescript = False
+
+def play_video(sid, cid, arch):
+    monitor = MyMonitor()
+    player = MyPlayer(sid=sid, cid=cid, arch=arch)
+    player.play_channel()
+    while not ( closescript ):
+        xbmc.sleep(500)
+    #xbmc.log('aborted!!!!!')
 
 def router(paramstring):
     """
@@ -79,14 +159,10 @@ def router(paramstring):
             sid = params['sid']
         else:
             sid = get_sid()
-        xbmc.log('in params: '+sid)
-        if params['action'] == 'listing':
-            # Display the list of videos in a provided category.
-            #list_videos(params['category'])
-            list_channels(sid)
-        elif params['action'] == 'play':
+        #xbmc.log('in params: '+sid)
+        if params['action'] == 'play':
             # Play a video from a provided URL.
-            play_video(params['video'])
+            play_video(sid, params['cid'], params['arch'])
         else:
             # If the provided paramstring does not contain a supported action
             # we raise an exception. This helps to catch coding errors,
@@ -96,7 +172,7 @@ def router(paramstring):
         # If the plugin is called from Kodi UI without any parameters,
         # display the list of video categories
         sid = get_sid()
-        xbmc.log('new: '+sid)
+        #xbmc.log('new: '+sid)
         list_channels(sid)
 
 if __name__ == '__main__':
