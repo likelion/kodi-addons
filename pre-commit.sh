72c2407f0f39f8668cdafea9a1e64a546c258970
Leonid Mokrushin <leonid.mokrushin@ericsson.com>
2017-01-10 16:03:24 +0100

    added changelog

--- pre-commit.sh
+++ pre-commit.sh
@@ -16,14 +16,11 @@ do
         zip -r "$addon_name/$addon_name-$version.zip" "$addon_name" -x "*.zip"
 done
 
-for f in ./repository.*
-do
-        echo "Repository found: $f"
-        version=`cat $f/addon.xml | sed -En 's/.*version="([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+)"/\\1/p' | tr -d '\015\032'`
-        addon_name=`basename $f`
-
-        rm -f "$addon_name-$version.zip"
-        zip -r "$addon_name-$version.zip" "$addon_name" -x "*.zip"
-done
+f=repository.likelion
+echo "Repository found: $f"
+version=`cat $f/addon.xml | sed -En 's/.*version="([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+)"/\\1/p' | tr -d '\015\032'`
+addon_name=`basename $f`
+rm -f "$addon_name-$version.zip"
+zip -r "$addon_name-$version.zip" "$addon_name" -x "*.zip"
 
 python addons_xml_generator.py
